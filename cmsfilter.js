e667d0e022bbeb5ddb8d7f6e6e00b3c82abf50a2
Josh Mkhari <njmkhari@gmail.com>
2024-05-08 10:02:38 +0200

    search updates

--- cmsfilter.js
+++ cmsfilter.js
@@ -882,43 +882,65 @@
         }]
     };
     var oe = (t, e, r, n) => (n && Yt(t), r ? !0 : e.every(o => Ur(t, o))),
-        //Console.log(Look Here)
-            Ur = (t, { filterKeys: e, values: r, match: n, mode: o, highlight: s, highlightCSSClass: i, elements: l }) => {
-              let a = [...r];
-              if (!a.length) return !0;
-              let m = e.includes("*");
-              m && (e = Object.keys(t.props));
-              let c = e.every(u => {
+        Ur = (t, {
+            filterKeys: e,
+            values: r,
+            match: n,
+            mode: o,
+            highlight: s,
+            highlightCSSClass: i,
+            elements: l
+        }) => {
+            let a = [...r];
+            if (!a.length) return !0;
+            let m = e.includes("*");
+            m && (e = Object.keys(t.props));
+            let c = e.filter(u => {
                 let p = t.props[u];
                 if (!p) return !1;
-                let { values: E, highlightData: f, type: T, range: d } = p, g = [...E];
+                let {
+                    values: E,
+                    highlightData: f,
+                    type: T,
+                    range: d
+                } = p, g = [...E];
                 if (!g.length) return !1;
                 if (o === "range") {
-                  let [C] = g, [v, S] = a, b = re(C, v, S, T);
-                  return b && s && (f == null || f.set(C, { highlightCSSClass: i })), b;
+                    let [C] = g, [v, S] = a, b = re(C, v, S, T);
+                    return b && s && (f == null || f.set(C, {
+                        highlightCSSClass: i
+                    })), b
                 }
-                let A = a.every(C => {
-                  if (d === "from" || d === "to") {
-                    let [S, b] = g, x = re(C, S, b, T);
-                    return x && s && (f == null || f.set(S, { highlightCSSClass: i }), f == null || f.set(b, { highlightCSSClass: i })), x;
-                  }
-                  return g.some(S => {
-                    let b;
-                    if (T === "date" && !m) {
-                      let [x, z] = [C, S].map(B => { var N; return (N = Tt(B)) == null ? void 0 : N.getTime() });
-                      b = x === z;
-                    } else {
-                      l.some(({ type: x }) => !["checkbox", "radio", "select-one"].includes(x)) || e.length > 1
-                        ? (b = S.toLowerCase().includes(C.toLowerCase()))
-                        : (b = C.toLowerCase() === S.toLowerCase());
+                let A = a.filter(C => {
+                    if (d === "from" || d === "to") {
+                        let [S, b] = g, x = re(C, S, b, T);
+                        return x && s && (f == null || f.set(S, {
+                            highlightCSSClass: i
+                        }), f == null || f.set(b, {
+                            highlightCSSClass: i
+                        })), x
                     }
-                    return b && s && (f == null || f.set(S, { highlightCSSClass: i, filterValue: C })), b;
-                  });
+                    return g.some(S => {
+                        let b;
+                        if (T === "date" && !m) {
+                            let [x, z] = [C, S].map(B => {
+                                var N;
+                                return (N = Tt(B)) == null ? void 0 : N.getTime()
+                            });
+                            b = x === z
+                        } else l.some(({
+                            type: x
+                        }) => !["checkbox", "radio", "select-one"].includes(x)) || e.length > 1 ? b = S.toLowerCase().includes(C.toLowerCase()) : b = C.toLowerCase() === S.toLowerCase();
+                        return b && s && (f == null || f.set(S, {
+                            highlightCSSClass: i,
+                            filterValue: C
+                        })), b
+                    })
                 });
-                return A;
-              });
-              return c;
-            },
+                return n === "all" ? A.length === a.length : A.length > 0
+            });
+            return n === "all" ? c.length === e.length : c.length > 0
+        },
         re = (t, e, r, n) => {
             let [o, s, i] = [t, e, r].map(l => n === "date" ? Tt(l) : Zt(l));
             return o ? !e && !Y(i) ? o <= i : !r && !Y(s) ? o >= s : Y(i) || Y(s) ? !1 : o >= s && o <= i : !1
@@ -1128,6 +1150,21 @@
                 return
             }
             for (let p of m) p.valid = oe(p, o, u, i);
+            if (m.every(p => !p.valid)) {
+                let searchQuery = '';
+                for (let { elements } of o) {
+                    for (let { element, type } of elements) {
+                        if (type === 'checkbox' || type === 'radio') {
+                            if (element.checked) {
+                                searchQuery += element.value + ' ';
+                            }
+                        } else {
+                            searchQuery += element.value + ' ';
+                        }
+                    }
+                }
+                console.log('Empty search result. Search query:', searchQuery.trim());
+            }
             e || (await n.switchPage(1, !1), n.scrollToAnchor(), a && ie(o), await Promise.all([(async () => {
                 u ? (await this.toggleFiltersState(u), await n.renderItems(!1, !c)) : (await n.renderItems(!1, !c), await this.toggleFiltersState(u))
             })(), (async () => {
